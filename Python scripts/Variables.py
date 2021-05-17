6ef1fe1cc6cb2dfcff0e24c2cd80606d4f28a48f
Aditya <aditya.daida@gmail.com>
2021-05-17 22:09:23 +0530

    variables

---
+++ Python scripts/Variables.py
@@ -0,0 +1,42 @@
+#variables
+one = 1
+two = 2
+three = 3
+print (one)
+print (two)
+print (three)
+two = 4 
+print (two)
+print (one)
+##
+Decimal = 1.1
+print (Decimal)
+
+##
+StringVar = "Hello" + "1"
+print(StringVar)
+
+##
+
+def FunctionName () :
+    global one 
+    newVar ="world"
+    print (one)
+    print (newVar)
+    return
+
+FunctionName()
+
+## 
+Five = 5
+print (Five)
+
+count = 0
+print (count)
+count = count + 1
+print (count)
+count = count + 1
+print (count)
+count = count * 3
+count += 4
+
